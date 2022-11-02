959127c957fe3f9c80c5761a3ae9b903d0af5e4a
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2022-11-02 20:45:40 -0300

    processo de deletar um article, incluindo rubcop e configurando

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,49 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit create update destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
       render :new
     end
-
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
       render :edit
     end
+  end
 
+  def destroy
+    return unless @article.destroy
+
+    redirect_to @article
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
